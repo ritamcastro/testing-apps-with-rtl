22296cda3ef64b0750f9e26a61e95eee8e37e9ed
Rita Castro <ritamcastro@gmail.com>
2020-05-13 17:28:44 +0100

    Add test for the form to get things started

--- tests/features/greetingz/welcome.test.jsx
+++ tests/features/greetingz/welcome.test.jsx
@@ -13,10 +13,16 @@ describe('Welcome screen', () => {
 
         describe('form to input your name', () => {
             it('contains a textbox to input the username', () => {
+                const { getByLabelText } = render(<Welcome />)
+                const input = getByLabelText(/what is your name/i)
+                expect(input).toBeInTheDocument()
+                expect(input).toBeRequired()
             })
 
             it('contains a button to submit the form that is disabled', () => {
-              
+                const { getByRole } = render(<Welcome />)
+                expect(getByRole('button', { name: /let's go!/i })).toBeInTheDocument()
+                expect(getByRole('button', { name: /let's go!/i })).toBeDisabled()
             })
         })
     })
