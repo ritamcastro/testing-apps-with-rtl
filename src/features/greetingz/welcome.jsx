8fe5f38e1312a12ac70a1eddf49ec6f97a8495b2
Rita Castro <ritamcastro@gmail.com>
2020-05-13 17:27:49 +0100

    Add implementation for title

--- src/features/greetingz/welcome.jsx
+++ src/features/greetingz/welcome.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Welcome = () => {
-    return null
+
+    return (
+        <div>
+            <h1>Welcome!</h1>
+        </div>
+    )
 }
 
 export default Welcome
