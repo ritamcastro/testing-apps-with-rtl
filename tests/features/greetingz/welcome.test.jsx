ad1385ed402df68cdc7b600f4ec16f13424c77a4
Rita Castro <ritamcastro@gmail.com>
2020-05-14 17:50:17 +0100

    Welcome screen specified

---
+++ tests/features/greetingz/welcome.test.jsx
@@ -0,0 +1,26 @@
+describe('Welcome screen', () => {
+
+    describe('initial render', () => {
+        it('contains the title', () => {
+            
+        })
+
+        describe('form to input your name', () => {
+            it('contains a textbox to input the username', () => {
+            })
+
+            it('contains a button to submit the form that is disabled', () => {
+              
+            })
+        })
+    })
+
+    it('calls the onSubmit with the username', () => {
+        // Given
+       
+
+        // When
+       
+        // Then
+    })
+})
