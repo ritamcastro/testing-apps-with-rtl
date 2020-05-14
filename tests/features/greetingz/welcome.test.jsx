5c1e8b77ac72a41a3c3c2d7a32d1485ff09a826a
Rita Castro <ritamcastro@gmail.com>
2020-05-14 17:52:02 +0100

    Add test for title

--- tests/features/greetingz/welcome.test.jsx
+++ tests/features/greetingz/welcome.test.jsx
@@ -1,8 +1,14 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Welcome from '../../../src/features/greetingz/welcome'
+ 
 describe('Welcome screen', () => {
 
     describe('initial render', () => {
         it('contains the title', () => {
-            
+            const { getByText } = render(<Welcome />)
+            expect(getByText('Welcome!')).toBeInTheDocument()
         })
 
         describe('form to input your name', () => {
@@ -18,7 +24,6 @@ describe('Welcome screen', () => {
     it('calls the onSubmit with the username', () => {
         // Given
        
-
         // When
        
         // Then
